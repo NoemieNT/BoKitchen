58a0df21ad077a17a5258178d21bf8bd747464a0
Noemie Nakab <noemie.nakab@gmail.com>
2019-12-05 13:16:18 +0100

    "new views with routes"

--- src/App.js
+++ src/App.js
@@ -2,17 +2,34 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 // import the views
 import Home from "./views/Home";
+import Cart from "./views/Cart";
+import Dashboard from "./views/Dashboard";
+import Menu from "./views/Menu";
+import MyAccount from "./views/MyAccount";
+import SignIn from "./views/SignIn";
+import SignUp from "./views/SignUp";
+
 // import component (view partials)
 import NavMain from "./components/NavMain";
 
 function App() {
   return (
-    <div className="App">
-      <NavMain />
-      <Switch>
-        <Route exact path="/" component={Home} />
-      </Switch>
-    </div>
+    <React.Fragment>
+      <header id="header-main">
+        <NavMain />
+      </header>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/cart" component={Cart} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="menu" component={Menu} />
+          <Route path="myaccount" component={MyAccount} />
+          <Route path="signIn" component={SignIn} />
+          <Route path="signUp" component={SignUp} />
+        </Switch>
+      </div>
+    </React.Fragment>
   );
 }
 
--- src/components/NavMain.jsx
+++ src/components/NavMain.jsx
@@ -1,14 +1,16 @@
-// import React from "react";
-// import { NavLink } from "react-router-dom";
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-// export default function NavMain() {
-//   return (
-//     <nav className="nav-main">
-//       <NavLink exact to="/">home</NavLink>
-//       <NavLink to="/all-students">all students</NavLink>
-//       <NavLink to="/all-cohorts">all cohorts</NavLink>
-//       <NavLink to="/create-student">+ student</NavLink>
-//       <NavLink to="/create-cohort">+ cohort</NavLink>
-//     </nav>
-//   );
-// }
+export default function NavMain() {
+  return (
+    <nav className="nav-main">
+      <NavLink exact to="/">
+        home
+      </NavLink>
+      {/* <NavLink to="/all-students">all students</NavLink>
+      <NavLink to="/all-cohorts">all cohorts</NavLink>
+      <NavLink to="/create-student">+ student</NavLink>
+      <NavLink to="/create-cohort">+ cohort</NavLink> */}
+    </nav>
+  );
+}
