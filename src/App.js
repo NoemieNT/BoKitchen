b7ea458f1966d6253583ce69f7121c68ecacbffc
Noemie Nakab <noemie.nakab@gmail.com>
2019-12-05 10:51:57 +0100

    3rd commit effacer des fichiers front

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 // import the views
 import Home from "./views/Home";
-import StudentsTable from "./views/StudentsTable";
-import StudentCreate from "./views/StudentCreate";
-import CohortsTable from "./views/CohortsTable";
-import CohortCreate from "./views/CohortCreate";
 // import component (view partials)
 import NavMain from "./components/NavMain";
 
@@ -15,10 +11,6 @@ function App() {
       <NavMain />
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/all-students" component={StudentsTable} />
-        <Route path="/all-cohorts" component={CohortsTable} />
-        <Route path="/create-student" component={StudentCreate} />
-        <Route path="/create-cohort" component={CohortCreate} />
       </Switch>
     </div>
   );
