a230e5bfedfbca19aa52c8fa0ee657b9e9d1d598
Noemie Nakab <noemie.nakab@gmail.com>
2019-12-06 16:03:13 +0100

    changes on general table

--- src/components/GeneralTable.jsx
+++ src/components/GeneralTable.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from "react";
 import ProductsTable from "./ProductsTable";
+import exampleFood from "../data/exampleFood.json";
 
 export class GeneralTable extends Component {
+  state = {
+    isActive: false,
+    foods: exampleFood
+  };
   render() {
     return (
       <div>
         <ProductsTable />
+        {/* <div className="column">
+          {this.state.foods.map((f, i) => (
+            <div key={i}></div>
+          ))}
+        </div> */}
       </div>
     );
   }
--- src/views/Menu.jsx
+++ src/views/Menu.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import FoodBox from "../components/FoodBox";
 import GeneralTable from "../components/GeneralTable";
 import ProductsTable from "../components/ProductsTable";
-import exampleFood from "../data/exampleFood.json";
+
 export class Menu extends Component {
   render() {
     return (
