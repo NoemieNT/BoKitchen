dac5f94e3e319885284368d860ee1ec64902e2c2
Noemie Nakab <noemie.nakab@gmail.com>
2019-12-06 15:27:27 +0100

    changements sur menu, ajout d'un bd json,

--- src/components/NavMain.jsx
+++ src/components/NavMain.jsx
@@ -3,14 +3,23 @@ import { NavLink } from "react-router-dom";
 
 export default function NavMain() {
   return (
-    <nav className="nav-main">
-      <NavLink exact to="/">
-        home
-      </NavLink>
-      <NavLink to="/menu">Menu</NavLink>
-      <NavLink to="/cart"> Cart </NavLink>
-      <NavLink to="/myaccount">My Account</NavLink>
-      <NavLink to="/SignIn">Sign In</NavLink>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul class="navbar-nav mr-auto mt-2 mt-md-0">
+          <li class="nav-item active">
+            <NavLink exact to="/">
+              {" "}
+              Home{" "}
+            </NavLink>
+          </li>
+          <li class="nav-item">
+            <NavLink to="/menu"> Menu </NavLink>
+            <NavLink to="/cart"> Cart </NavLink>
+            <NavLink to="/myaccount"> My Account</NavLink>
+            <NavLink to="/SignIn"> Sign In </NavLink>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
