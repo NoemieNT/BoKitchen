4c12f67b5a80800b9c0e3ddda4e183e6bbcbcae1
Hanna Mohamed <hanna@MacBook-Pro-de-Hanna.local>
2019-12-10 15:07:35 +0100

    cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -5,15 +5,43 @@ export class Cart extends Component {
   render() {
     return (
       <div>
-        <h1>i'm cart</h1>
+        <h1>Cart</h1>
         {!this.props.products.length && <p>no product selected yet...</p>}
-        {Boolean(this.props.products.length) &&
-          this.props.products.map((p, i) => (
-            <p key={i}>
-              {p.quantity} {this.props.foods[p.index].name} :{" "}
-              {this.props.foods[p.index].price}$
-            </p>
-          ))}
+        {Boolean(this.props.products.length) && (
+          <table className="table-borderless">
+            <thead>
+              <tr>
+                <th scope="col">Qty</th>
+                <th scope="col">Name</th>
+                <th scope="col">PU</th>
+                <th scope="col">Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.props.products.map((p, i) => (
+                <tr key={i}>
+                  <td>{p.quantity}</td>
+                  <td>{this.props.foods[p.index].name}</td>
+                  <td>
+                    {this.props.foods[p.index].price}
+                    <spans>$</spans>
+                  </td>
+                  <td>
+                    {this.props.foods[p.index].price * p.quantity}
+                    <spans>$</spans>
+                  </td>
+                </tr>
+              ))}
+              <td class="table-active" colSpan="4">
+                Total :
+                {[...this.props.products]
+                  .map(p => p.quantity * this.props.foods[p.index].price)
+                  .reduce((prev, next) => prev + next)}
+                $
+              </td>
+            </tbody>
+          </table>
+        )}
         <form onSubmit={this.handleFormSubmit}>
           <button className="btn btn-default" type="submit">
             Save
